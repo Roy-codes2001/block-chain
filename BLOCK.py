45980d3788e503c642092694af4337f5214d748b
Mitrashis Basu Roy <92039336+Roy-codes2001@users.noreply.github.com>
2021-10-16 21:14:25 +0530

    Add files via upload

---
+++ BLOCK.py
@@ -0,0 +1,23 @@
+import datetime
+import hashlib
+
+
+class Block:
+    def __init__(self, previous_block_hash, data, timestamp):
+        self.previous_block_hash = previous_block_hash
+        self.data = data
+        self.timestamp = timestamp
+        self.hash = self.get_hash()
+
+    @staticmethod
+    def genesis_block():
+        return Block("0", "0", datetime.datetime.now())
+
+    def get_hash(self):
+        header_bin = (str(self.previous_block_hash) +
+                      str(self.data) +
+                      str(self.timestamp))
+
+        hash_inside = hashlib.sha256(header_bin.encode()).hexdigest().encode()
+        hash_outside = hashlib.sha256(hash_inside).hexdigest()
+        return hash_outside
